5fdaef9c9866fa05a6c4dca7381d603afacba409
Mr Dung <dung13890@Mrs-MacBook-Pro.local>
2021-05-27 11:10:37 +0700

    init project

---
+++ go/go.mod
@@ -0,0 +1,10 @@
+module github.com/dao.anh.dung/grpc/serve
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5
+	google.golang.org/grpc v1.38.0
+	google.golang.org/protobuf v1.26.0
+)
---
+++ go/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	pb "github.com/dao.anh.dung/grpc/serve/pb/user"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type server struct {
+	request []*pb.GetRequest
+}
+
+func (s *server) GetUsers(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, error) {
+	users := &pb.GetResponse{
+		Status: "success",
+		Users: []*pb.User{
+			&pb.User{
+				Id:   "1",
+				Name: req.Keyword,
+			},
+		},
+	}
+	return users, nil
+}
+
+func main() {
+	// tcp listener
+	lis, err := net.Listen("tcp", ":51308")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	srv := grpc.NewServer()
+	pb.RegisterUserServer(srv, &server{})
+	srv.Serve(lis)
+}
